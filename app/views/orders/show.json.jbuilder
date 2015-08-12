24276d0dd1ec447870226bf22381372374682ed3
damir <damirikq@gmail.com>
2015-08-12 15:00:51 +0300

    added order model

---
+++ app/views/orders/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @order, :id, :title, :desc, :image, :price, :time, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811123009) do
+ActiveRecord::Schema.define(version: 20150811150130) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20150811123009) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "title"
+    t.text     "desc"
+    t.string   "image"
+    t.float    "price"
+    t.integer  "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
